391d50b8ee8d5e65b3fd15c04d64deca69fd3ccc
Siarhei Lapich <svlapich@gmail.com>
2020-02-14 16:41:13 +0300

    create department-managment-dao

---
+++ department-managment-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package com.epam.brest.courses.dao;
+
+
+
+import com.epam.brest.courses.model.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    List<Department> getDepartments();
+
+}
