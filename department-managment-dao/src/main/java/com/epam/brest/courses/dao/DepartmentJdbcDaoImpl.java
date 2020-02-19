ef473075b3f4fc19c551e8a93fe2d41a72a903a2
Siarhei Lapich <svlapich@gmail.com>
2020-02-19 23:03:40 +0300

    DepartmentJdbcDaoImpl

---
+++ department-managment-dao/src/main/java/com/epam/brest/courses/dao/DepartmentJdbcDaoImpl.java
@@ -0,0 +1,61 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.Department;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DepartmentJdbcDaoImpl implements DepartmentDao {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentJdbcDaoImpl.class);
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public DepartmentJdbcDaoImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<Department> getDepartments() {
+        LOGGER.debug("Get all departments");
+
+        List<Department> departments = namedParameterJdbcTemplate
+                .query("SELECT d.departmentId, d.departmentName FROM department d ORDER BY d.departmentName", new DepartmentRowMapper());
+        return departments;
+    }
+
+    @Override
+    public Department getDepartmentById(Integer departmentId) {
+        return null;
+    }
+
+    @Override
+    public Department addDepartment(Department department) {
+        return null;
+    }
+
+    @Override
+    public void updateDepartment(Department department) {
+
+    }
+
+    @Override
+    public void deleteDepartment(Integer departmentId) {
+
+    }
+
+    private class DepartmentRowMapper implements RowMapper<Department> {
+        @Override
+        public Department mapRow(ResultSet resultSet, int i) throws SQLException {
+            Department department = new Department();
+            department.setDepartmentId(resultSet.getInt("DEPARTMENT_ID"));
+            department.setDepartmentName(resultSet.getString("DEPARTMENT_NAME"));
+            return department;
+        }
+    }
+}
